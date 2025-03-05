1a73b41fefab60f32d8430dd0c86de3f3e28e098
Guigaskt55 <guilhermihenrique1@gmail.com>
2025-03-04 22:30:36 -0300

    Delete modulos directory

--- modulos/Lista_comida.py
+++
@@ -1,3 +0,0 @@
-bebidas = ['coca-cola','pepsi','agua','suco']
-comidas = ['Pizza', 'Hambúrguer', 'Arroz', 'Feijão']
-doces = ['Bolo', 'Sorvete', 'Chocolate', 'Pudim']
--- modulos/Trunch(Sem arredondamentos).py
+++
@@ -1,5 +0,0 @@
-import math
-#Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira
-num=float(input('\033[35mescolha um número com casas decimais (ex:16.99)\033[m'))
-trunc=math.trunc(num)
-print(f'\033[31m{trunc}\033[m')
