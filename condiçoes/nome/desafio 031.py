1c8b621534c7663366a76edb311c5c50d094c1fd
Guigaskt55 <guilhermihenrique1@gmail.com>
2025-03-04 22:35:02 -0300

    Delete condiçoes/nome/desafio 031.py

--- condiçoes/nome/desafio 031.py
+++
@@ -1,9 +0,0 @@
-#Desenvolva um programa que pergunte a distancia de uma viagem em KM.Calcule o preço da passagem. Cobrando R$0,50 por KM para viagens até 200KM e R$0,45 para viagens mais longas
-Viagem=(float(input('Qual a distancia de sua viagem ?')))
-
-if Viagem <= 200:
-    preço=Viagem*0.50
-else:
-    preço=Viagem*0.45
-    
-print(f'sua passagem final será {preço:.2f} ')
