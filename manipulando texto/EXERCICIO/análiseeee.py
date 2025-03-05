ae15f96387199f87944b48e1d8f0351467c48f8a
Guigaskt55 <guilhermihenrique1@gmail.com>
2025-03-04 22:30:45 -0300

    Delete manipulando texto directory

--- manipulando texto/EXERCICIO/análiseeee.py
+++
@@ -1,7 +0,0 @@
-cidade=(input('\033[36mfale o nome de uma cidade :\033[m '))
-começa_com_santo= cidade.lower().startswith(('santo','santa')) # startswith verifica se o texto começa com 'santo' ou nao. dentro do startwith quanto mais opção mais () () ()
-if começa_com_santo :
-    print("\033[32ma cidade começa com 'santo' ou 'santa .\033[m")
-else :
-    print("\033[31ma cidade não começa com 'santo' ou 'santa' .\033[m")
-    
